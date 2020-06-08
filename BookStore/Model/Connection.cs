f72eb9b2b06a22f241fa16d70baa780f35c9ff9a
tommyblasta@gmail.com <tommyblasta@gmail.com>
2020-06-08 12:53:47 +0200

    First commit Adds Login and UserMain windows

---
+++ BookStore/Model/Connection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace BookStore
+{
+    class Connection
+    {
+        public SqlConnectionStringBuilder builder { get; set; }
+        public Connection()
+        {
+            builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = @"192.168.0.105,1433\SQLTOM";
+            builder.IntegratedSecurity = false;
+            builder.UserID = "ruf";
+            builder.Password = "ruf";
+            
+        }
+    }
+}
---
+++ SQL/CreateBooks.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [Books](
+	[Id] int PRIMARY KEY IDENTITY (1,1),
+	[Name] varchar(70),
+	[Author] varchar(70),
+	[]
+)
