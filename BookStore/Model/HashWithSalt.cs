cb44dfbb39f85fca342f6eb2c86bf8a8eaed4b88
tommyblasta@gmail.com <tommyblasta@gmail.com>
2020-06-10 14:13:29 +0200

    Added class to handle password hashing

---
+++ BookStore/Model/HashWithSalt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Model
+{
+    public class HashWithSalt
+    {
+        public string Salt { get; set; }
+        public string Hash { get; set; }
+    }
+}
---
+++ BookStore/Model/PasswordHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookStore;
+using BookStore.ModelView;
+
+namespace BookStore.Model
+{
+    /// <summary>
+    /// Static methods to handle passwords
+    /// </summary>
+    static class PasswordHandler
+    {
+        /// <summary>
+        /// Accepts a string with user inputed password and returns a randomly generated salt
+        /// hashsum of the combination.
+        /// </summary>
+        /// <param name="pass">User inputed password</param>
+        /// <returns></returns>
+        static HashWithSalt HashAndSaltPass(string pass)
+        {
+
+            return new HashWithSalt { Hash = "", Salt = "" };
+        }
+        static UserInfo ConfirmPassword (string userName, string pass)
+        {
+            return new UserInfo();
+        }
+    }
+}
